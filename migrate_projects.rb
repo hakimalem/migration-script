66c2399908a356a681894688d18f9a6c7e96811f
hakimalem <76614346+hakimalem@users.noreply.github.com>
2025-07-18 10:36:15 +0200

    Update migrate_projects.rb

--- migrate_projects.rb
+++ migrate_projects.rb
@@ -1,56 +1,113 @@
 require 'json'
 require 'rest-client'
 
-API_URL = 'https://data.earthportal.eu/projects'
-API_KEY = 'API_KEY' 
+BASE_URL = 'https://datastage.earthportal.eu'
+API_URL = "#{BASE_URL}/projects"
+API_KEY = '9a3f9f33-f512-4a04-bb84-45636068e255'
+BACKUP_FILE = '/home/hakim/Desktop/earthport_projects_backup.json'
 
-def fetch_old_projects
-  response = RestClient.get(API_URL, {accept: :json})
-  JSON.parse(response.body)
+def fix_acronym(acronym)
+  acronym = acronym.upcase.gsub(/\s+/, '_')
+  acronym = acronym.gsub(/[^A-Z0-9_-]/, '')
+  acronym
+end
+
+def fix_creator_references(creators)
+  return [] if creators.nil? || creators.empty?
+  
+  creators.map do |creator_url|
+    if creator_url.include?('/users/')
+      username = creator_url.split('/').last
+      username
+    else
+      nil
+    end
+  end.compact
+end
+
+def fix_ontology_references(ontologies)
+  return ["#{BASE_URL}/ontologies/SWEET"] if ontologies.nil? || ontologies.empty?
+  
+  ontologies.map do |ontology_url|
+    if ontology_url.include?('/ontologies/')
+      ontology_url.gsub("https://data.earthportal.eu", BASE_URL)
+    else
+      nil
+    end
+  end.compact
 end
 
 def build_new_project(old)
+  fixed_acronym = fix_acronym(old["acronym"])
+  fixed_creators = fix_creator_references(old["creator"])
+  fixed_ontologies = fix_ontology_references(old["ontologyUsed"])
+
   {
-    "acronym"      => old["acronym"],
+    "acronym"      => fixed_acronym,
     "type"         => "FundedProject",
     "name"         => old["name"],
     "homePage"     => old["homePage"],
     "description"  => old["description"],
-    "ontologyUsed" => (old["ontologyUsed"].is_a?(Array) && old["ontologyUsed"].any?) ? old["ontologyUsed"] : ["https://data.earthportal.eu/ontologies/SWEET"],
+    "ontologyUsed" => fixed_ontologies,
     "created"      => old["created"],
     "updated"      => old["updated"],
-    "keywords"     => [old["acronym"]],
-    "contact"      => nil,
-    "organization" => nil,
+    "keywords"     => [fixed_acronym],
+    "contact"      => old["contacts"],
+    "organization" => old["institution"],
     "grant_number" => nil,
     "start_date"   => nil,
     "end_date"     => nil,
     "funder"       => nil,
     "logo"         => nil,
     "source"       => nil,
-    "creator"      => old["creator"]
+    "creator"      => fixed_creators
   }
 end
 
-def update_project(new_project, id)
-  RestClient.put(
-    "#{API_URL}/#{id}",
-    new_project.to_json,
-    {content_type: :json, accept: :json, Authorization: "apikey token=#{API_KEY}"}
-  )
+def create_project(project)
+  begin
+    puts "Attempting to create project: #{project['acronym']}..."
+    response = RestClient.post(
+      API_URL,
+      project.to_json,
+      {
+        content_type: :json, 
+        accept: :json, 
+        Authorization: "apikey token=#{API_KEY}"
+      }
+    )
+    puts "Created project: #{project['acronym']}"
+    return true
+  rescue RestClient::ExceptionWithResponse => e
+    puts "Failed to create #{project['acronym']}: #{e.response}"
+    puts "Project data: #{project.to_json}"
+    return false
+  rescue => e
+    puts "Error creating #{project['acronym']}: #{e.message}"
+    return false
+  end
 end
 
-# Main migration
-old_projects = fetch_old_projects
-old_projects.each do |old|
-  new_project = build_new_project(old)
-  id = old["id"].split('/').last
-  begin
-    update_project(new_project, id)
-    puts "Migrated #{new_project['acronym']}"
-  rescue RestClient::ExceptionWithResponse => e
-    puts "Failed to migrate #{new_project['acronym']}: #{e.response}"
-  rescue => e
-    puts "Failed to migrate #{new_project['acronym']}: #{e.message}"
+puts "Loading projects from #{BACKUP_FILE}..."
+if File.exist?(BACKUP_FILE)
+  projects = JSON.parse(File.read(BACKUP_FILE))
+  
+  success_count = 0
+  fail_count = 0
+  
+  projects.each do |old_project|
+    new_project = build_new_project(old_project)
+    
+    if create_project(new_project)
+      success_count += 1
+    else
+      fail_count += 1
+    end
   end
+  
+  puts "Creation completed!"
+  puts "Successfully created: #{success_count} projects"
+  puts "Failed to create: #{fail_count} projects"
+else
+  puts "Backup file not found: #{BACKUP_FILE}"
 end
